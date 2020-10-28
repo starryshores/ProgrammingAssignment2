8673ba2d9b5e13cc77f5e6f1c5662e0c5f2c7857
starryshores <brianac18@gmail.com>
2020-10-28 16:52:54 -0500

    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function below takes a matrix (x) which is invertible
+## and caches the matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()) {{
+  z=NULL            ## NULL promotes the object to an empty list
+  set=function(p) { 
+    x<<-p
+    z<<-NULL       
+    ## using <<- instead of <- sets z as NULL within the
+    ## environment of the function only.  z doesn't not exist
+    ## as NULL in the global environment.
+  }
+  get<-function()x                       
+  setinvr<-function(inverse) z<<-inverse  
+  getinvr<-function() z                   
+  list(set=set, get=get, setinvr=setinvr, getinvr=getinvr)
+}
+  
 }
 
+## The function below returns the inverse of the matrix
+## which was cached by makeCacheMatrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...) { 
+  invr=x$getinvr()
+  if(!is.null(invr)){       ## if inverse is not null
+    print("Getting cached data.")
+    return(invr)
+    
+  }
+  
+  matrix.data=x$get()
+  invr=solve(matrix.data,...) #solve function to inverse matrix
+  x$setinvr(invr)
+  return(invr)
 }
